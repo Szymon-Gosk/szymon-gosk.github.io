44793e55556ef28acf2c088bc0d9a8b5a0e8721c
Szymon-Gosk <szymongosk767@gmail.com>
2020-10-21 18:42:49 +0200

    First commit

--- js/przyrost-naturalny/jquery-plugin.js
+++ js/przyrost-naturalny/jquery-plugin.js
@@ -1,5 +1,38 @@
-var name = $("#name").html();
+$(document).ready(function() {
 
-output = $("#output").html();
- 
-output.$("#output").replaceWith("<h1> Hello </h1>");
+    // przycisk 'Zatwierdź'
+    $('#submit-button').click(function() {
+
+        // inicjalizacja zmiennych
+        let inputs = [];
+        let empty = 0;
+
+        // Zebranie wartości z inputów
+        $('.input').each(function () {
+            if($(this).val() === "") {
+                empty += 1;
+            } else {
+                inputs.push($(this).val());
+            }
+        });
+
+        // Sprawdzenie czy są przynajmniej 3 lata TODO
+        if(empty > 2) {
+
+        }
+
+        // (100000, 2017)
+
+        // zamiana stringów na inty
+        inputs = inputs.map(x=>+x);
+
+        let sum = 0;
+        for(let i = 0; i < inputs.length; i++) {
+            sum += inputs[i];
+        }
+
+        $('#wyniki-temp').text(sum);
+
+    });
+
+});
