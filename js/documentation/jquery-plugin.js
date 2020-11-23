700b33e697df74f8d358306e085790da745bc22f
Szymon-Gosk <szymongosk767@gmail.com>
2020-11-23 15:32:01 +0100

    Documentation attached to documentation section of the page / reworked first algorithm

---
+++ js/documentation/jquery-plugin.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    PDFObject.embed("../rsc/documentation.pdf", "#pdf", {height: "80vh", width: "80vw"});
+
+});
+
--- js/przyrost-naturalny/jquery-plugin.js
+++ js/przyrost-naturalny/jquery-plugin.js
@@ -3,143 +3,158 @@ $(document).ready(function() {
     // przycisk 'Zatwierdź'
     $('#submit-button').click(function() {
 
-        $('#d-1-2').css('display', 'flex');
-
         // inicjalizacja zmiennych
-        let inputs = [];
-        let years = [];
-        let empty = 0;
+        let inputsI = [];
+        let yearsI = [];
+        let x = parseInt($('#year-6').val());
+        let isDataCorrect = true;
 
         // Zebranie wartości z inputów
         $('.input-y').each(function () {
-            if($(this).val() !== "") {
-                years.push($(this).val());
-            }
-
+            yearsI.push($(this).val());
         });
 
         $('.input-v').each(function () {
-            if($(this).val() !== "") {
-                inputs.push($(this).val());
-            }
+            inputsI.push($(this).val());
         });
 
+        for(let i = 0; i < 5; i++) {
+            isDataCorrect = isDataCorrect &&
+                !((inputsI[i] === "" && yearsI[i] !== "") || (inputsI[i] !== "" && yearsI[i] === ""));
+        }
+
+        $('#d-1-2').css('display', 'flex');
+
         // Sprawdzenie czy są przynajmniej 3 lata TODO
-        if(empty > 2) {
+        if(!isDataCorrect || isNaN(x)) {
+            $('#polynomial').html('Uzupełnij pola poprawnymi zmiennymi!');
+            $('#result').html('Przynajmniej 3 lata i rok do wyliczenia');
+        } else {
 
-        }
+            let inputs = [];
+            let years = [];
 
-        // zamiana stringów na inty
-        let values = [];
-        inputs = inputs.map(x => +x);
-        years = years.map(x => +x);
-
-        for (let i = 0; i < inputs.length; i++) {
-            let temp = [];
-            temp[0] = years[i];
-            temp[1] = inputs[i];
-            values[i] = temp
-        }
-
-        // values[X][0] - X-th year       (x)
-        // values[X][1] - X-th value      (y)
-        let N = values.length;
-        let b = [];
-
-        b[0] = values[0][1];
-    
-        for(let k = 1; k <= N-1; k++) {
-
-            let sum = values[0][1];
-            let denominator = 1;
-
-            for(let i = 1; i <= k-1; i++) {
-                let product = 1;
-                for(let j = 0; j < i; j++) {
-                    product *= (values[k][0] - values[j][0]);
+            inputsI.forEach(element => {
+                if(element !== "") {
+                    inputs.push(element);
                 }
-                sum += b[i]*product;
+            });
+
+            yearsI.forEach(element => {
+                if(element !== "") {
+                    years.push(element);
+                }
+            });
+
+            // zamiana stringów na inty
+            let values = [];
+            inputs = inputs.map(x => +x);
+            years = years.map(x => +x);
+
+            for (let i = 0; i < inputs.length; i++) {
+                let temp = [];
+                temp[0] = years[i];
+                temp[1] = inputs[i];
+                values[i] = temp
             }
 
-            for(let i = 0; i <= k-1; i++) {
-                denominator *= (values[k][0] - values[i][0]);
+            // values[X][0] - X-th year       (x)
+            // values[X][1] - X-th value      (y)
+            let N = values.length;
+            let b = [];
+
+            b[0] = values[0][1];
+
+            for(let k = 1; k <= N-1; k++) {
+
+                let sum = values[0][1];
+                let denominator = 1;
+
+                for(let i = 1; i <= k-1; i++) {
+                    let product = 1;
+                    for(let j = 0; j < i; j++) {
+                        product *= (values[k][0] - values[j][0]);
+                    }
+                    sum += b[i]*product;
+                }
+
+                for(let i = 0; i <= k-1; i++) {
+                    denominator *= (values[k][0] - values[i][0]);
+                }
+
+                b[k] = (values[k][1] - sum)/denominator;
             }
 
-            b[k] = (values[k][1] - sum)/denominator;
-        }
+            // Wielomian Newtona
 
-        // Wielomian Newtona
+            let polynomial = "P(x) = " + b[0];
+            let polynomial_graph = "" + b[0];
 
-        let polynomial = "P(x) = " + b[0];
-        let polynomial_graph = "" + b[0];
-
-        for(let k = 1; k <= N-1; k++) {
-            if(b[k] < 0) {
-                polynomial = polynomial + " - " + Math.abs(b[k]);
-                polynomial_graph = polynomial_graph + " - " + Math.abs(b[k]);
-            } else {
-                polynomial = polynomial + " + " + b[k];
-                polynomial_graph = polynomial_graph + " + " + b[k];
+            for(let k = 1; k <= N-1; k++) {
+                if(b[k] < 0) {
+                    polynomial = polynomial + " - " + Math.abs(b[k]);
+                    polynomial_graph = polynomial_graph + " - " + Math.abs(b[k]);
+                } else {
+                    polynomial = polynomial + " + " + b[k];
+                    polynomial_graph = polynomial_graph + " + " + b[k];
+                }
+                for(let i = 0; i <= k-1; i++) {
+                    polynomial = polynomial + "(x - " + values[i][0] + ")";
+                    polynomial_graph = polynomial_graph + "*(x - " + values[i][0] + ")";
+                }
             }
-            for(let i = 0; i <= k-1; i++) {
-                polynomial = polynomial + "(x - " + values[i][0] + ")";
-                polynomial_graph = polynomial_graph + "*(x - " + values[i][0] + ")";
+
+            $("#polynomial").html(polynomial);
+
+            // Wyliczenie
+
+            let P = b[0];
+
+            for (let k = 1; k <= N - 1; k++) {
+                let ingredient = b[k];
+                for (let i = 0; i <= k - 1; i++) {
+                    ingredient *= (x - values[i][0]);
+                    polynomial = polynomial + "(x - " + values[i][0] + ")";
+                }
+                P += ingredient;
             }
-        }
 
-        $("#polynomial").html(polynomial);
+            P = Math.round(P * 100) / 100;
 
-        // Wyliczenie
+            $('#result').html('P(' + x + ') = ' + P);
 
-        let P = b[0];
-        let x = parseInt($('#year-6').val());
+            // Wykres
 
-        for (let k = 1; k <= N - 1; k++) {
-            let ingredient = b[k];
-            for (let i = 0; i <= k - 1; i++) {
-                ingredient *= (x - values[i][0]);
-                polynomial = polynomial + "(x - " + values[i][0] + ")";
+            // TODO
+
+            var board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox:[-5000,5000,5000,-5000], axis:true});
+
+            // Macro function plotter
+            function addCurve(board, func, atts) {
+                return board.create('functiongraph', [func], atts);
             }
-            P += ingredient;
-        }
 
-        P = Math.round(P * 100) / 100;
-
-        $('#result').html('P(' + x + ') = ' + P);
-
-        // Wykres
-
-        // TODO
-
-        var board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox:[-5000,5000,5000,-5000], axis:true});
-
-        // Macro function plotter
-        function addCurve(board, func, atts) {
-            return board.create('functiongraph', [func], atts);
-        }
-
-        // Simplified plotting of function
-        function plot(func, atts) {
-            if (atts==null) {
-                return addCurve(board, func, {strokewidth:2});
-            } else {
-                return addCurve(board, func, atts);
+            // Simplified plotting of function
+            function plot(func, atts) {
+                if (atts==null) {
+                    return addCurve(board, func, {strokewidth:2});
+                } else {
+                    return addCurve(board, func, atts);
+                }
             }
+
+            let func = 'function f(x) { ' +
+                '   return ' + polynomial_graph + '; ' +
+                '} ' +
+                'c = plot(f);';
+
+            // Usage of the macro
+            function doIt() {
+                eval(func);
+            }
+
+            doIt();
         }
-
-        let func = 'function f(x) { ' +
-            '   return ' + polynomial_graph + '; ' +
-            '} ' +
-            'c = plot(f);';
-
-        // Usage of the macro
-        function doIt() {
-            console.log(':)');
-            eval(func);
-        }
-
-        doIt();
-
     });
 
 });
